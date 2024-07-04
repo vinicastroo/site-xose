7b7fcab437389a8194ed0167c2cb26d7b9b4c0bf
Vinicius Castro <viniciusc.d.c@hotmail.com>
2024-07-04 16:59:32 -0300

    feat/build

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -16,8 +16,9 @@ const notable = Notable({
 })
 
 export const metadata: Metadata = {
-  title: 'Guilherme Schulze',
-  description: 'Fotografo e videomaker',
+  title: 'Guilherme Schulze - Fotógrafo e Videomaker em Rio do Sul',
+  description:
+    'Guilherme Schulze é fotógrafo e videomaker em Rio do Sul, especializado em capturar momentos únicos e memoráveis desde 2018. Com formação em Produção Multimídia e paixão por cinema, oferece serviços personalizados de fotografia e produção de vídeo para eventos, ensaios e projetos comerciais. Transforme suas memórias.',
 }
 
 export default function RootLayout({
