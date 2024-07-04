073f2744ea2fdb0e0b072cbd291a629fa08d84b8
Vinicius Castro <viniciusc.d.c@hotmail.com>
2024-07-04 10:35:00 -0300

    feat/build

--- web/src/utils/getImages.ts
+++ web/src/utils/getImages.ts
@@ -1,6 +1,9 @@
 import fs from 'fs'
 import path from 'path'
 
+const shuffle = (array: string[]) => {
+  return array.sort(() => Math.random() - 0.5)
+}
 export function getImages() {
   const imagesDir = path.join(process.cwd(), 'public/projetos')
   const imageFiles = fs.readdirSync(imagesDir).filter((file) => {
@@ -8,5 +11,5 @@ export function getImages() {
     return ext === '.jpg' || ext === '.jpeg' || ext === '.png' || ext === '.gif'
   })
 
-  return imageFiles.map((file) => `/projetos/${file}`)
+  return shuffle(imageFiles.map((file) => `/projetos/${file}`))
 }
