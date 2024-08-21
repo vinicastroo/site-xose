577df97954a612c6b93850663c4f8afe7d02094c
Vinicius Castro <viniciusc.d.c@hotmail.com>
2024-08-21 15:27:47 -0300

    feat/change banner

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Notable } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
 import 'react-photo-view/dist/react-photo-view.css'
+import Script from 'next/script'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -33,6 +34,27 @@ export default function RootLayout({
           {children}
           <Analytics />
         </div>
+
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1272151067480129');
+        fbq('track', 'PageView');
+        `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1`}
+          />
+        </noscript>
       </body>
     </html>
   )
