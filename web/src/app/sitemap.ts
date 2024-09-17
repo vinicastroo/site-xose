9cceffdbff622c71af148cd6587c34daa6ebf920
Vinicius Castro <viniciusc.d.c@hotmail.com>
2024-09-16 22:05:07 -0300

    feat/change photos

--- web/src/app/sitemap.ts
+++ web/src/app/sitemap.ts
@@ -1,7 +1,32 @@
-import { MetadataRoute } from 'next'
+import api from '@/services/api'
+import type { EventsProps } from './portifolio/page'
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+interface Metadata {
+  url: string
+  lastModified?: string | Date
+  changeFrequency?: string
+  priority?: number
+}
+
+async function getAllProjects() {
+  const response = await api.get(`/events?populate[0]=thumb&sort=order`)
+
+  const events: EventsProps[] = response.data
+  const formattedEvents: Metadata[] = events.map((event) => {
+    return {
+      url: `${process.env.BASE_API_URL}/portifolio/${event.id}`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    }
+  })
+
+  return formattedEvents
+}
+
+export default async function sitemap() {
+  const events = await getAllProjects()
+  const defaultPaths = [
     {
       url: 'https://www.guilhermeschulze.com.br/',
       lastModified: new Date(),
@@ -15,4 +40,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
   ]
+
+  return [...defaultPaths, ...events]
 }
