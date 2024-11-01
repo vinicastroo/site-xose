8de5bfc951493d183d3e9ce61ab3aa9f870b301c
Vinicius Castro <viniciusc.d.c@hotmail.com>
2024-11-01 15:58:48 -0300

    feat/chance text alt

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <head>
         <meta
           name="facebook-domain-verification"
